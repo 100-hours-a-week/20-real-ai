fb962d2de610b58fec4353ab3abd0ff3301f9f7d
이수인 Suin Lee <0420@kookmin.ac.kr>
2025-05-01 16:50:55 +0900

    ♻️ refactor: 임베딩 모델 사이즈 변경

--- app/core/embedding_model.py
+++ app/core/embedding_model.py
@@ -2,5 +2,5 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 # RAG에서 사용할 다국어 임베딩 모델 정의
 embedder = HuggingFaceEmbeddings(
-    model_name="intfloat/multilingual-e5-medium-instruct"
+    model_name="intfloat/multilingual-e5-large"
 )
