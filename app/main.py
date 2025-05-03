18e9430c9800fabfd70b86c28077ae202200db88
shinnyriver <gd12091@gmail.com>
2025-05-03 23:07:34 +0900

    ✨ feat: CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from app.api.router import api_router
 from app.core.error_handler import add_exception_handlers
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://test.kakaotech.com",
+    "https://dev.kakaotech.com",
+    "https://www.kakaotech.com",
+    "https://kakaotech.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 전체 API 라우터 등록
 app.include_router(api_router)
