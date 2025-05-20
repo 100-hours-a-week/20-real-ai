85e17166be0075826cad30cff3da728e02e7ecd7
seoyeon-u <seoyeon.u730@gmail.com>
2025-05-20 14:37:09 +0900

    ✨ feat : ChatResponse에 conversation_id 필드 추가
    - 첫 요청 시 에러 방지를 위해 ChatRequest에 Optional[str] 타입의 conversation_id 을 추가했다는 주석
    - 대화 세션 관리를 위해 ChatResponse에도 conversation_id 필드 추가

--- app/schemas/chat_schema.py
+++ app/schemas/chat_schema.py
@@ -4,7 +4,7 @@ from typing import Optional
 class ChatRequest(BaseModel):
     question: str
     user_id : str
-    conversation_id: Optional[str] = None
+    conversation_id: Optional[str] = None # 첫 요청 시 에러 방지
 
 class ChatAnswer(BaseModel):
     answer: str
@@ -12,3 +12,4 @@ class ChatAnswer(BaseModel):
 class ChatResponse(BaseModel):
     message: str
     data: ChatAnswer
+    conversation_id: str
