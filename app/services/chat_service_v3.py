01eb7a43a8df180624b1e6f9d92c03e9c11879ea
seoyeon-u <seoyeon.u730@gmail.com>
2025-06-12 10:29:30 +0900

    ✨ feat: BM25 + FAISS 기반 앙상블 리트리버 구성 함수 추가

--- app/services/chat_service_v3.py
+++ app/services/chat_service_v3.py
@@ -6,11 +6,39 @@ from app.models.llm_client import get_chat_response_stream
 from langsmith.run_helpers import get_current_run_tree
 from langsmith import traceable
 from app.models.safety_model import classify_prompt
+from langchain.retrievers import BM25Retriever, EnsembleRetriever
+from scripts.create_vectorstore import header_splitted_docs, vectorstore
 
-# 상위 2개의 문서를 검색하는 retriever 구성
-retriever = load_vectorstore().as_retriever(
-        search_type="similarity_score_threshold",  
-        search_kwargs={"score_threshold": 0.7, "k": 2}
+# BM25 리트리버 생성 함수
+def create_bm25_retriever(header_splitted_docs):
+    # 텍스트 기반 검색기
+    bm25_retriever = BM25Retriever.from_documents(
+        header_splitted_docs,
+        )
+    bm25_retriever.k = 1
+    return bm25_retriever
+
+# FAISS 리트리버 생성 함수
+def create_faiss_retriever(vectorstore):  
+    # 벡터 기반 검색기
+    retriever = vectorstore.as_retriever(
+        search_type="similarity_score_threshold",         # 유사도 점수 기반 필터링
+        search_kwargs={"k": 2, "score_threshold": 0.7}
+    )
+    return retriever
+
+# 앙상블 리트리버 생성 함수
+def create_ensemble_retriever(retrievers, weights=[0.7, 0.3]):
+    ensemble_retriever = EnsembleRetriever(
+        retrievers=retrievers,
+        weights=weights
+    )
+    return ensemble_retriever
+
+# 최종 리트리버 생성
+retriever = create_ensemble_retriever(
+    [create_bm25_retriever(header_splitted_docs), create_faiss_retriever(vectorstore)],
+    weights=[0.7, 0.3]
     )
 
 # 사용자별 세션 히스토리에 Q/A message 저장 
