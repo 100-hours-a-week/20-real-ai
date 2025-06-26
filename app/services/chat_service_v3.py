27d3aff729a6858405af4bb67ba5504167310fc3
seoyeon-u <seoyeon.u730@gmail.com>
2025-06-26 09:43:24 +0900

    ✨ feat: 벡터DB 마이그레이션 및 최근 문서 필터링 기능 구현

--- app/services/chat_service_v3.py
+++ app/services/chat_service_v3.py
@@ -11,10 +11,10 @@ from langchain.schema import Document
 from scripts.qdrant.create_vectorstore import client, vectorstore, header_splitted_docs
 
 # 앙상블 리트리버
-def create_bm25_retriever(header_splitted_docs):
+def create_bm25_retriever(selected_docs):
     # 텍스트 기반 검색기
     bm25_retriever = BM25Retriever.from_documents(
-        header_splitted_docs,
+        selected_docs,
         )
     bm25_retriever.k = 1
     return bm25_retriever
@@ -33,12 +33,6 @@ def create_ensemble_retriever(retrievers, weights):
         weights=weights
     )
     return ensemble_retriever
-
-# 최종 리트리버 생성
-retriever = create_ensemble_retriever(
-    [create_bm25_retriever(header_splitted_docs), create_qdrant_retriever(vectorstore)],
-    weights=[0.3, 0.7]
-    )
 
 # Qdrant 필터링
 def get_documents_by_qdrant(client, collection_name: str, tz: str = 'Asia/Seoul'):
@@ -100,22 +94,19 @@ async def chat_service_stream(question: str, request_id: str, userId: int):
     history = get_session_history(userId)
     history_str = chat_history_to_string(history)
 
-    docs = retriever.get_relevant_documents(parsed_question)
-    context = "\n\n".join([doc.page_content for doc in docs])
-
     # "최근" "최신" 키워드 포함된 질문이면 → Qdrant에서 최신 문서 3개 조회
     if date_filter.get("mode") == "latest":
-        recently_docs = get_documents_by_qdrant(client, "ChoonChat")
-        qdrant_docs = qdrant_records_to_documents(recently_docs)
-        metadata = "\n\n".join([
-        f"제목: {doc.metadata.get('Header 1', '없음')}\n"
-        f"날짜: {doc.metadata.get('date', '없음')}\n"
-        f"내용: {doc.page_content}"
-        for doc in qdrant_docs
-    ])
-
+        selected_raw_docs = get_documents_by_qdrant(client, "ChoonChat")
+        selected_docs = qdrant_records_to_documents(selected_raw_docs)
     else:
-      metadata = ""
+        selected_docs = header_splitted_docs
+
+    retriever = create_ensemble_retriever(
+    [create_bm25_retriever(selected_docs), create_qdrant_retriever(vectorstore)],
+    weights=[0.3, 0.7]
+    )
+    docs = retriever.get_relevant_documents(parsed_question)
+    context = "\n\n".join([doc.page_content for doc in docs])
 
     # 검색 결과가 없다면 스트리밍 종료 및 안내 메시지 출력
     if not docs:
@@ -124,7 +115,7 @@ async def chat_service_stream(question: str, request_id: str, userId: int):
         return
 
     # 프롬프트 정의 및 LLM 호출
-    prompt = chatbot_rag_prompt.format(history=history_str, context=context, metadata=metadata, question=parsed_question)
+    prompt = chatbot_rag_prompt.format(history=history_str, context=context, question=parsed_question)
 
     # vLLM 스트리밍 API 호출
     agen = get_chat_response_stream(prompt, docs, request_id)
